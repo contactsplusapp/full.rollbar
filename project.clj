2b0b43efae95b9adae8722eb87464608c120253c
Karlis Lauva <k@karlis.me>
2016-07-01 11:01:14 -0600

    Version 0.10.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fullcontact/full.rollbar "0.10.1"
+(defproject fullcontact/full.rollbar "0.10.2-SNAPSHOT"
   :description "Library to ship exceptions and request information to the rollbar logging service."
   :url "https://github.com/fullcontact/full.rollbar"
   :license {:name "Eclipse Public License - v 1.0"
