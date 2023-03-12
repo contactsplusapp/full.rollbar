6c6c964fa77f07f680302545fbee1911c0af5a30
Mor Gazith <mor.gazith@benchmarkemail.com>
2023-03-12 10:31:04 +0200

    CON-1333 / CON-1564 / Remove ahead-of-time flag from all in-use FullContact libraries

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   {:url (str "https://contactsplus.jfrog.io/artifactory/" path)
    :sign-releases false})
 
-(defproject fullcontact/full.rollbar "0.10.6-SNAPSHOT"
+(defproject fullcontact/full.rollbar "0.10.7"
   :description "Library to ship exceptions and request information to the rollbar logging service."
   :url "https://github.com/contactsplusapp/full.rollbar"
   :license {:name "Eclipse Public License - v 1.0"
@@ -14,10 +14,9 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/core.async "0.7.559"]
                  [fullcontact/camelsnake "0.9.0"]
-                 [fullcontact/full.async "0.9.0"]
-                 [fullcontact/full.core "0.10.1" :exclusions [org.clojure/clojurescript]]
-                 [fullcontact/full.http "1.0.8"]]
-  :aot :all
+                 [fullcontact/full.async "1.1.1"]
+                 [fullcontact/full.core "1.1.3" :exclusions [org.clojure/clojurescript]]
+                 [fullcontact/full.http "1.0.11"]]
   :release-tasks [["vcs" "assert-committed"]
                   ["change" "version" "leiningen.release/bump-version" "release"]
                   ["vcs" "commit"]
@@ -27,5 +26,5 @@
                   ["vcs" "commit"]
                   ["vcs" "push"]]
   :plugins [[lein-midje "3.1.3"]]
-  :profiles {:dev {:dependencies [[midje "1.7.0"]]}})
+  :profiles {:dev {:dependencies [[midje "1.10.9"]]}})
 
